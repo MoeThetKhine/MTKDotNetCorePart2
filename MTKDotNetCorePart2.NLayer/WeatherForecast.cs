1dd6a87f3fcb649ad1369ad5aca6c3a795d23bc2
Moe Thet Khine <soniapoudel03@gmail.com>
2024-07-18 13:00:11 +0630

    Add project files.

---
+++ MTKDotNetCorePart2.NLayer/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace MTKDotNetCorePart2.NLayer
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
