4541347ab5f398112ce3be106a48d67ebcba16b4
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-19 23:31:17 +0630

    -

--- MTKDotNetCorePart2.NLayer/Model/BlogModel.cs
+++ MTKDotNetCorePart2.NLayer/Model/BlogModel.cs
@@ -1,13 +1,12 @@
-﻿namespace MTKDotNetCorePart2.NLayer.Model
+﻿namespace MTKDotNetCorePart2.NLayer.Model;
+
+[Table("Tbl_Blog")]
+public class BlogModel
 {
-    [Table("Tbl_Blog")]
-    public class BlogModel
-    {
-        [Key]
-        public long BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-        public bool IsActive { get; set; }
-    }
+    [Key]
+    public long BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+    public bool IsActive { get; set; }
 }
