50ed59c2ce02f7a04f62b6ea280e0d7d17c4cb06
Moe Thet Khine <soniapoudel03@gmail.com>
2024-07-19 23:16:56 +0630

    HttpClient

---
+++ MTKDotNetCorePart2.HttpClientApi/Database/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MTKDotNetCorePart2.HttpClientApi.Model;
+
+namespace MTKDotNetCorePart2.HttpClientApi.Database
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionStrings._sqlConnectionStringBuilder.ConnectionString);
+        }
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
---
+++ MTKDotNetCorePart2.HttpClientConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("Hello, World!");
+ // ConsoleApp - Client(Frontend)
+ // Asp.Net Core Web Api - Server(Backend)
+
+HttpClient client = new HttpClient();
+var response = await client.GetAsync("https://localhost:7013/api/blog");
+
+if (response.IsSuccessStatusCode)
+{
+    string jsonStr = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(jsonStr);
+    List<BlogDto> lst = JsonConvert.DeserializeObject<List<BlogDto>>(jsonStr)!;
+    foreach(var blog in lst)
+    {
+        Console.WriteLine(JsonConvert.SerializeObject(blog));
+    }
+}
+
+public class BlogDto
+{
+    public long BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+    public bool IsActive { get; set; }
+}
+
+
