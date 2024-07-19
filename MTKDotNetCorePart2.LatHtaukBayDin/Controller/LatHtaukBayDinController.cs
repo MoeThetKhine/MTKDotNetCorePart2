757b9ef9d6c8e2db06b1ed6490fe8bb5e3d5fe3e
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-19 23:27:23 +0630

    Format LatHtaukBayDinController

--- MTKDotNetCorePart2.LatHtaukBayDin/Controller/LatHtaukBayDinController.cs
+++ MTKDotNetCorePart2.LatHtaukBayDin/Controller/LatHtaukBayDinController.cs
@@ -1,60 +1,60 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
-namespace MTKDotNetCorePart2.LatHtaukBayDin.Controller
+namespace MTKDotNetCorePart2.LatHtaukBayDin.Controller;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LatHtaukBayDinController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class LatHtaukBayDinController : ControllerBase
+    private async Task<LatHtaukBayDin> GetDataAsync()
     {
-        private async Task<LatHtaukBayDin> GetDataAsync()
-        {
-            string jsonStr = await System.IO.File.ReadAllTextAsync("data.json");
-            var model = JsonConvert.DeserializeObject<LatHtaukBayDin>(jsonStr);
-            return model;
-        }
+        string jsonStr = await System.IO.File.ReadAllTextAsync("data.json");
+        var model = JsonConvert.DeserializeObject<LatHtaukBayDin>(jsonStr);
 
-        [HttpGet("questions")]
+        return model!;
+    }
 
-        public async Task<IActionResult> Questions()
-        {
-           var model = await GetDataAsync();
-            return Ok(model.questions);
-        }
-        [HttpGet("numberlist")]
-        public async Task<IActionResult> NumberList()
-        {
-            var model = await GetDataAsync();
-            return Ok(model.numberList);
-        }
-      
-        [HttpGet("{questionNo}/{no}")]
-        public async Task<IActionResult>Answers(int questionNo,int no)
-        {
-            var model = await GetDataAsync();
-            return Ok(model.answers.FirstOrDefault(x=> x.questionNo == questionNo && x.answerNo == no));
-        }
+    [HttpGet("questions")]
+    public async Task<IActionResult> Questions()
+    {
+        var model = await GetDataAsync();
+        return Ok(model.questions);
+    }
 
-        public class LatHtaukBayDin
-        {
-            public Question[] questions { get; set; }
-            public Answer[] answers { get; set; }
-            public string[] numberList { get; set; }
-        }
+    [HttpGet("number-list")]
+    public async Task<IActionResult> NumberList()
+    {
+        var model = await GetDataAsync();
+        return Ok(model.numberList);
+    }
 
-        public class Question
-        {
-            public int questionNo { get; set; }
-            public string questionName { get; set; }
-        }
+    [HttpGet("{questionNo}/{no}")]
+    public async Task<IActionResult> Answers(int questionNo, int no)
+    {
+        var model = await GetDataAsync();
+        return Ok(
+            model.answers.FirstOrDefault(x => x.questionNo == questionNo && x.answerNo == no)
+        );
+    }
 
-        public class Answer
-        {
-            public int questionNo { get; set; }
-            public int answerNo { get; set; }
-            public string answerResult { get; set; }
-        }
+    public class LatHtaukBayDin
+    {
+        public Question[] questions { get; set; }
+        public Answer[] answers { get; set; }
+        public string[] numberList { get; set; }
+    }
 
+    public class Question
+    {
+        public int questionNo { get; set; }
+        public string questionName { get; set; }
+    }
+
+    public class Answer
+    {
+        public int questionNo { get; set; }
+        public int answerNo { get; set; }
+        public string answerResult { get; set; }
     }
 }
