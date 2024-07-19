bd5c31c152ffbe13d1036d6a1736e2a197b9dc0d
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-19 23:24:57 +0630

    Format Code

--- MTKDotNetCorePart2.HttpClientConsoleApp/Program.cs
+++ MTKDotNetCorePart2.HttpClientConsoleApp/Program.cs
@@ -1,9 +1,10 @@
-﻿using Newtonsoft.Json;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
 
 Console.WriteLine("Hello, World!");
- // ConsoleApp - Client(Frontend)
- // Asp.Net Core Web Api - Server(Backend)
+
+// ConsoleApp - Client(Frontend)
+// Asp.Net Core Web Api - Server(Backend)
 
 HttpClient client = new HttpClient();
 var response = await client.GetAsync("https://localhost:7013/api/blog");
@@ -13,7 +14,7 @@ if (response.IsSuccessStatusCode)
     string jsonStr = await response.Content.ReadAsStringAsync();
     Console.WriteLine(jsonStr);
     List<BlogDto> lst = JsonConvert.DeserializeObject<List<BlogDto>>(jsonStr)!;
-    foreach(var blog in lst)
+    foreach (var blog in lst)
     {
         Console.WriteLine(JsonConvert.SerializeObject(blog));
     }
@@ -27,5 +28,3 @@ public class BlogDto
     public string BlogContent { get; set; }
     public bool IsActive { get; set; }
 }
-
-
