0ebe65a41cc3015fe30d5d56ac89e04d0544f462
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-19 23:29:37 +0630

    Format BlogController

--- MTKDotNetCorePart2.NLayer/Features/BlogController.cs
+++ MTKDotNetCorePart2.NLayer/Features/BlogController.cs
@@ -1,75 +1,86 @@
-﻿namespace MTKDotNetCorePart2.NLayer.Features
+﻿namespace MTKDotNetCorePart2.NLayer.Features;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : ControllerBase
+    private readonly BL_Blog _bL_Blog;
+
+    public BlogController(BL_Blog bL_Blog)
     {
-        private readonly BL_Blog _bL_Blog;
+        _bL_Blog = bL_Blog;
+    }
 
-        public BlogController(BL_Blog bL_Blog)
-        {
-            _bL_Blog = bL_Blog;
-        }
-        [HttpGet]
-        public IActionResult Read()
-        {
-            var lst = _bL_Blog.GetBlogs();
-            return Ok(lst);
-        }
-        [HttpGet("{id}")]
-        public IActionResult Edit(int id)
-        {
-            var item = _bL_Blog.GetBlog(id);
-            if(item is null)
-            {
-                return NotFound("No data found");
-            }
-            return Ok(item);
-        }
-        [HttpPost]
-        public IActionResult Create(BlogModel blog)
-        {
-            var result = _bL_Blog.CreateBlog(blog);
+    [HttpGet]
+    public IActionResult Read()
+    {
+        var lst = _bL_Blog.GetBlogs();
+        return Ok(lst);
+    }
 
-            string message = result > 0 ? "Saving Successful" : "Saving Failed";
-            return Ok(message);
-        }
-        [HttpPut("{id}")]
-        public IActionResult Update(int id,BlogModel blog)
+    [HttpGet("{id}")]
+    public IActionResult Edit(int id)
+    {
+        var item = _bL_Blog.GetBlog(id);
+        if (item is null)
         {
-            var item = _bL_Blog.GetBlog(id);
-            if(item is null)
-            {
-                return NotFound("No data found");
-            }
-            var result = _bL_Blog.UpdateBlog(id,blog);
-            string message = result > 0 ? "Updating Successful" : "Updating Fail";
-            return Ok(message);
-        }
-        [HttpPatch("{id}")]
-        public IActionResult Patch(int  id,BlogModel blog)
-        {
-            var item = _bL_Blog.GetBlog(id);
-            if(item is null)
-            {
-                return NotFound("No data found");
-            }
-            var result = _bL_Blog.PatchBlog(id,blog);
-            string message = result > 0 ? "Updating Successful" : "Updating Fail";
-            return Ok(message); 
+            return NotFound("No data found");
         }
 
-        [HttpDelete("{id}")]    
-        public IActionResult Delete(int id)
+        return Ok(item);
+    }
+
+    [HttpPost]
+    public IActionResult Create(BlogModel blog)
+    {
+        var result = _bL_Blog.CreateBlog(blog);
+        string message = result > 0 ? "Saving Successful" : "Saving Failed";
+
+        return Ok(message);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, BlogModel blog)
+    {
+        var item = _bL_Blog.GetBlog(id);
+        if (item is null)
         {
-            var item = _bL_Blog.GetBlog(id);
-            if(item is null)
-            {
-                return NotFound("No data found");
-            }
-            var result = _bL_Blog.DeleteBlog(id);
-            string message = result > 0 ? "Deleting Successful" : "Deleting Failed.";
-            return Ok(message);
+            return NotFound("No data found");
         }
+
+        var result = _bL_Blog.UpdateBlog(id, blog);
+        string message = result > 0 ? "Updating Successful" : "Updating Fail";
+
+        return Ok(message);
+    }
+
+    [HttpPatch("{id}")]
+    public IActionResult Patch(int id, BlogModel blog)
+    {
+        var item = _bL_Blog.GetBlog(id);
+        if (item is null)
+        {
+            return NotFound("No data found");
+        }
+
+        var result = _bL_Blog.PatchBlog(id, blog);
+        string message = result > 0 ? "Updating Successful" : "Updating Fail";
+
+        return Ok(message);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var item = _bL_Blog.GetBlog(id);
+        if (item is null)
+        {
+            return NotFound("No data found");
+        }
+
+        var result = _bL_Blog.DeleteBlog(id);
+        string message = result > 0 ? "Deleting Successful" : "Deleting Failed.";
+
+        return Ok(message);
     }
 }
