c735a8b9cbcd1a4aec241f10eddadf908e3e8878
Moe Thet Khine <soniapoudel03@gmail.com>
2024-07-19 19:21:43 +0630

    -

--- MTKDotNetCorePart2.NLayer/Database/GlobalUsings.cs
+++ MTKDotNetCorePart2.NLayer/Database/GlobalUsings.cs
@@ -1,3 +1,5 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using MTKDotNetCorePart2.NLayer.Model;
 global using System.Data.SqlClient;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using System.ComponentModel.DataAnnotations;
