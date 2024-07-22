c601a90f138d1c106cc24eed366eefdc5a6d3639
Moe Thet Khine <soniapoudel03@gmail.com>
2024-07-22 21:56:20 +0630

    [Complete PizzaApi]

--- MTKDotNetCorePart2.PizzaApi/Database/AppDbContext.cs
+++ MTKDotNetCorePart2.PizzaApi/Database/AppDbContext.cs
@@ -9,6 +9,12 @@ namespace MTKDotNetCorePart2.PizzaApi.Database
         {
             optionsBuilder.UseSqlServer(ConnectionStrings._sqlConnectionStringBuilder.ConnectionString);
         }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+         : base(options)
+        {
+        }
+
         public DbSet<PizzaModel> Pizzas { get; set; }
         public DbSet<PizzaExtraModel>PizzaExtras { get; set; }
         public DbSet<PizzaOrderDetailModel>PizzaOrderDetails { get; set; }
