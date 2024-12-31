c71aed64e8afbf5e5954d01293f62cadcc956fc9
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 19:57:10 +0630

    Format Code

--- MTKDotNetCorePart2.HttpClientApi/Database/ConnectionStrings.cs
+++ MTKDotNetCorePart2.HttpClientApi/Database/ConnectionStrings.cs
@@ -1,13 +1,12 @@
-﻿namespace MTKDotNetCorePart2.HttpClientApi.Database
+﻿namespace MTKDotNetCorePart2.HttpClientApi.Database;
+
+public class ConnectionStrings
 {
-    public class ConnectionStrings
+    public static SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
     {
-        public static SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
-        {
-            DataSource = "localhost", // or "(local)" or your server name
-            InitialCatalog = "OJTBatch1",
-            IntegratedSecurity = true,
-            TrustServerCertificate = true,
-        };
-    }
+        DataSource = "localhost", // or "(local)" or your server name
+        InitialCatalog = "OJTBatch1",
+        IntegratedSecurity = true,
+        TrustServerCertificate = true,
+    };
 }
