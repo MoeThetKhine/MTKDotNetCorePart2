353fe4c07030c6e071e369e5d2e71899710ea064
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 20:54:41 +0630

    -

--- MTKDotNetCorePart2.PizzaApi/Model/OrderResquestModel.cs
+++ MTKDotNetCorePart2.PizzaApi/Model/OrderResquestModel.cs
@@ -1,7 +1,11 @@
 ﻿namespace MTKDotNetCorePart2.PizzaApi.Model;
 
+#region OrderResquestModel
+
 public class OrderResquestModel
 {
     public int PizzaId {  get; set; }
     public int[] Extras { get; set; }
 }
+
+#endregion
