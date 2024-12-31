287a8da3d2e9585e6b2496702b87ef867df6ae65
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 20:54:23 +0630

    Format Code

--- MTKDotNetCorePart2.PizzaApi/Model/OrderResquestModel.cs
+++ MTKDotNetCorePart2.PizzaApi/Model/OrderResquestModel.cs
@@ -1,8 +1,7 @@
-﻿namespace MTKDotNetCorePart2.PizzaApi.Model
+﻿namespace MTKDotNetCorePart2.PizzaApi.Model;
+
+public class OrderResquestModel
 {
-    public class OrderResquestModel
-    {
-        public int PizzaId {  get; set; }
-        public int[] Extras { get; set; }
-    }
+    public int PizzaId {  get; set; }
+    public int[] Extras { get; set; }
 }
