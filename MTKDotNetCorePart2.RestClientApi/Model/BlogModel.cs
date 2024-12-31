05eb98658db19581cb3daf310558dff50ca1677b
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 21:39:08 +0630

    Format Code

--- MTKDotNetCorePart2.RestClientApi/Model/BlogModel.cs
+++ MTKDotNetCorePart2.RestClientApi/Model/BlogModel.cs
@@ -1,13 +1,12 @@
-﻿namespace MTKDotNetCorePart2.RestClientApi.Model
+﻿namespace MTKDotNetCorePart2.RestClientApi.Model;
+
+[Table("Tbl_Blog")]
+public class BlogModel
 {
-    [Table("Tbl_Blog")]
-    public class BlogModel
-    {
-        [Key]
-        public long BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-        public bool IsActive { get; set; }
-    }
+    [Key]
+    public long BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+    public bool IsActive { get; set; }
 }
