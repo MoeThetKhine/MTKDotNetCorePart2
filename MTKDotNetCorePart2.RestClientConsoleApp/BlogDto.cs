1de3003ba87cfa8281ec34e24ae8c95bb5c2748e
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 21:48:17 +0630

    -

--- MTKDotNetCorePart2.RestClientConsoleApp/BlogDto.cs
+++ MTKDotNetCorePart2.RestClientConsoleApp/BlogDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MTKDotNetCorePart2.RestClientConsoleApp
+﻿namespace MTKDotNetCorePart2.RestClientConsoleApp
 {
     public class BlogDto
     {
