44d912468562f395bf07eb2002938d97d80c42b0
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 20:25:10 +0630

    -

--- MTKDotNetCorePart2.NLayer/Model/BlogModel.cs
+++ MTKDotNetCorePart2.NLayer/Model/BlogModel.cs
@@ -1,5 +1,7 @@
 ﻿namespace MTKDotNetCorePart2.NLayer.Model;
 
+#region BlogModel
+
 [Table("Tbl_Blog")]
 public class BlogModel
 {
@@ -10,3 +12,5 @@ public class BlogModel
     public string BlogContent { get; set; }
     public bool IsActive { get; set; }
 }
+
+#endregion
