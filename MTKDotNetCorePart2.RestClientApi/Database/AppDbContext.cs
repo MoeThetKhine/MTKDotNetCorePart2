c249252fc703ed5114f4d081024d67f7923eb16e
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 21:41:11 +0630

    Format Code

--- MTKDotNetCorePart2.RestClientApi/Database/AppDbContext.cs
+++ MTKDotNetCorePart2.RestClientApi/Database/AppDbContext.cs
@@ -1,13 +1,11 @@
-﻿namespace MTKDotNetCorePart2.RestClientApi.Database
-{
-    public class AppDbContext : DbContext
-    {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(ConnectionStrings._sqlConnectionStringBuilder.ConnectionString);
-        }
+﻿namespace MTKDotNetCorePart2.RestClientApi.Database;
 
-        public DbSet<BlogModel> Blogs { get; set; }
+public class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(ConnectionStrings._sqlConnectionStringBuilder.ConnectionString);
     }
 
+    public DbSet<BlogModel> Blogs { get; set; }
 }
