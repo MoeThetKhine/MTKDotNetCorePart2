ab9b28d6c5b82b91befe2e9cee377cf3121ba925
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 20:50:18 +0630

    Feat : Add GlobalUsing

--- MTKDotNetCorePart2.PizzaApi/GlobalUsing.cs
+++ MTKDotNetCorePart2.PizzaApi/GlobalUsing.cs
@@ -1,2 +1,4 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using MTKDotNetCorePart2.PizzaApi.Database;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
