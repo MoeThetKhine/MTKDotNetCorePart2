3ea1963c57f99f78b7c20f5ff4dd3eb65dfd437e
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 21:29:50 +0630

    -

--- MTKDotNetCorePart2.PizzaApiDapperService/Database/ConnectionStrings.cs
+++ MTKDotNetCorePart2.PizzaApiDapperService/Database/ConnectionStrings.cs
@@ -1,13 +1,17 @@
 ﻿namespace MTKDotNetCorePart2.PizzaApiDapperService.Database;
 
+#region ConnectionStrings
+
 public class ConnectionStrings
 {
     public static SqlConnectionStringBuilder _sqlConnectionStringBuilder =
-        new()
-        {
-            DataSource = "localhost",
-            InitialCatalog = "OJTBatch1",
-            IntegratedSecurity = true,
-            TrustServerCertificate = true,
-        };
+    new()
+    {
+        DataSource = "localhost",
+        InitialCatalog = "OJTBatch1",
+        IntegratedSecurity = true,
+        TrustServerCertificate = true,
+    };
 }
+
+#endregion
