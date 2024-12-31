437f970d6a07651265b2bed53b8d6c77db2589e9
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 20:58:58 +0630

    Format Code

--- MTKDotNetCorePart2.PizzaApi/Database/ConnectionStrings.cs
+++ MTKDotNetCorePart2.PizzaApi/Database/ConnectionStrings.cs
@@ -1,14 +1,13 @@
-﻿namespace MTKDotNetCorePart2.PizzaApi.Database
+﻿namespace MTKDotNetCorePart2.PizzaApi.Database;
+
+public class ConnectionStrings
 {
-    public class ConnectionStrings
-    {
-        public static SqlConnectionStringBuilder _sqlConnectionStringBuilder =
-            new()
-            {
-                DataSource = "localhost",
-                InitialCatalog = "OJTBatch1",
-                IntegratedSecurity = true,
-                TrustServerCertificate = true,
-            };
-    }
+    public static SqlConnectionStringBuilder _sqlConnectionStringBuilder =
+        new()
+        {
+            DataSource = "localhost",
+            InitialCatalog = "OJTBatch1",
+            IntegratedSecurity = true,
+            TrustServerCertificate = true,
+        };
 }
