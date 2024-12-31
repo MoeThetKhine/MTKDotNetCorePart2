236cdb9aa40bdf9610b90e39c1042118b78e581d
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 21:30:26 +0630

    Format Code

--- MTKDotNetCorePart2.PizzaApiDapperService/Database/AppDbContext.cs
+++ MTKDotNetCorePart2.PizzaApiDapperService/Database/AppDbContext.cs
@@ -1,15 +1,13 @@
-﻿namespace MTKDotNetCorePart2.PizzaApiDapperService.Database
-{
-    public class AppDbContext : DbContext
-    {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(ConnectionStrings._sqlConnectionStringBuilder.ConnectionString);
-        }
-        public DbSet<PizzaModel> Pizzas { get; set; }
-        public DbSet<PizzaExtraModel> PizzaExtras { get; set; }
-        public DbSet<PizzaOrderDetailModel> PizzaOrderDetails { get; set; }
-        public DbSet<PizzaOrderModel> PizzaOrders { get; set; }
-    }
+﻿namespace MTKDotNetCorePart2.PizzaApiDapperService.Database;
 
+public class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(ConnectionStrings._sqlConnectionStringBuilder.ConnectionString);
+    }
+    public DbSet<PizzaModel> Pizzas { get; set; }
+    public DbSet<PizzaExtraModel> PizzaExtras { get; set; }
+    public DbSet<PizzaOrderDetailModel> PizzaOrderDetails { get; set; }
+    public DbSet<PizzaOrderModel> PizzaOrders { get; set; }
 }
