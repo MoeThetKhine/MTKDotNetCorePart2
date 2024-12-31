746d24d6de5f1456544ffeb27f83c22dadae67b7
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 20:13:53 +0630

    Format Code

--- MTKDotNetCorePart2.MyanmarProverbsApi/Controllers/MyanmarProverbsController.cs
+++ MTKDotNetCorePart2.MyanmarProverbsApi/Controllers/MyanmarProverbsController.cs
@@ -1,88 +1,86 @@
-﻿namespace MTKDotNetCorePart2.MyanmarProverbsApi.Controllers
+﻿namespace MTKDotNetCorePart2.MyanmarProverbsApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MyanmarProverbsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MyanmarProverbsController : ControllerBase
+    private async Task<Tbl_Mmproverbs> GetDataFromApi()
     {
-        private async Task<Tbl_Mmproverbs> GetDataFromApi()
+        //HttpClient client = new HttpClient();
+        //var response = await client.GetAsync("https://raw.githubusercontent.com/sannlynnhtun-coding/Myanmar-Proverbs/main/MyanmarProverbs.json");
+        //if (response.IsSuccessStatusCode)
+        //{
+        //    string jsonStr = await response.Content.ReadAsStringAsync();
+        //    var model =  JsonConvert.DeserializeObject<Tbl_Mmproverbs>(jsonStr);
+        //    return model;
+        //}
+        //return null;
+
+        var jsonStr = await System.IO.File.ReadAllTextAsync("data.json");
+        var model = JsonConvert.DeserializeObject<Tbl_Mmproverbs>(jsonStr);
+        return model;
+    }
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var model = await GetDataFromApi();
+        return Ok(model.Tbl_MMProverbsTitle);
+    }
+
+    [HttpGet("{titleName}")]
+    public async Task<IActionResult> Get(string titleName)
+    {
+        var model = await GetDataFromApi();
+        var item = model.Tbl_MMProverbsTitle.FirstOrDefault(x => x.TitleName == titleName);
+        if (item == null)
         {
-            //HttpClient client = new HttpClient();
-            //var response = await client.GetAsync("https://raw.githubusercontent.com/sannlynnhtun-coding/Myanmar-Proverbs/main/MyanmarProverbs.json");
-            //if (response.IsSuccessStatusCode)
-            //{
-            //    string jsonStr = await response.Content.ReadAsStringAsync();
-            //    var model =  JsonConvert.DeserializeObject<Tbl_Mmproverbs>(jsonStr);
-            //    return model;
-            //}
-            //return null;
-
-            var jsonStr = await System.IO.File.ReadAllTextAsync("data.json");
-            var model = JsonConvert.DeserializeObject<Tbl_Mmproverbs>(jsonStr);
-            return model;
+            return NotFound();
         }
-        [HttpGet]
-        public async Task<IActionResult> Get()
+        var titleId = item.TitleId;
+        var result = model.Tbl_MMProverbs.Where(x => x.TitleId == titleId);
+
+        List<Tbl_MmproverbsHead> lst = result.Select(x => new Tbl_MmproverbsHead
         {
-            var model = await GetDataFromApi();
-            return Ok(model.Tbl_MMProverbsTitle);
-        }
+            ProverbId = x.ProverbId,
+            ProverbName = x.ProverbName,
+            TitleId = x.TitleId,
+        }).ToList();
 
-        [HttpGet("{titleName}")]
-        public async Task<IActionResult> Get(string titleName)
-        {
-            var model = await GetDataFromApi();
-            var item = model.Tbl_MMProverbsTitle.FirstOrDefault(x => x.TitleName == titleName);
-            if (item == null)
-            {
-                return NotFound();
-            }
-            var titleId = item.TitleId;
-            var result = model.Tbl_MMProverbs.Where(x => x.TitleId == titleId);
-
-            List<Tbl_MmproverbsHead> lst = result.Select(x => new Tbl_MmproverbsHead
-            {
-                ProverbId = x.ProverbId,
-                ProverbName = x.ProverbName,
-                TitleId = x.TitleId,
-            }).ToList();
-
-            return Ok(lst);
-        }
-        [HttpGet("{titleId}/{proverbId}")]
-        public async Task<IActionResult> Get(int titleId,int proverbId)
-        {
-            var model = await GetDataFromApi();
-            var item = model.Tbl_MMProverbs.FirstOrDefault(x => x.TitleId ==titleId && x.ProverbId == proverbId);
-
-            return Ok(item);
-        }
+        return Ok(lst);
     }
-
-    public class Tbl_Mmproverbs
+    [HttpGet("{titleId}/{proverbId}")]
+    public async Task<IActionResult> Get(int titleId,int proverbId)
     {
-        public Tbl_Mmproverbstitle[] Tbl_MMProverbsTitle { get; set; }
-        public Tbl_MmproverbsDetails[] Tbl_MMProverbs { get; set; }
-    }
+        var model = await GetDataFromApi();
+        var item = model.Tbl_MMProverbs.FirstOrDefault(x => x.TitleId ==titleId && x.ProverbId == proverbId);
 
-    public class Tbl_Mmproverbstitle
-    {
-        public int TitleId { get; set; }
-        public string TitleName { get; set; }
+        return Ok(item);
     }
+}
 
-    public class Tbl_MmproverbsDetails
-    {
-        public int TitleId { get; set; }
-        public int ProverbId { get; set; }
-        public string ProverbName { get; set; }
-        public string ProverbDesp { get; set; }
-    }
-    public class Tbl_MmproverbsHead
-    {
-        public int TitleId { get; set; }
-        public int ProverbId { get; set; }
-        public string ProverbName { get; set; }
-      
-    }
+public class Tbl_Mmproverbs
+{
+    public Tbl_Mmproverbstitle[] Tbl_MMProverbsTitle { get; set; }
+    public Tbl_MmproverbsDetails[] Tbl_MMProverbs { get; set; }
+}
 
+public class Tbl_Mmproverbstitle
+{
+    public int TitleId { get; set; }
+    public string TitleName { get; set; }
+}
+
+public class Tbl_MmproverbsDetails
+{
+    public int TitleId { get; set; }
+    public int ProverbId { get; set; }
+    public string ProverbName { get; set; }
+    public string ProverbDesp { get; set; }
+}
+public class Tbl_MmproverbsHead
+{
+    public int TitleId { get; set; }
+    public int ProverbId { get; set; }
+    public string ProverbName { get; set; }
+  
 }
