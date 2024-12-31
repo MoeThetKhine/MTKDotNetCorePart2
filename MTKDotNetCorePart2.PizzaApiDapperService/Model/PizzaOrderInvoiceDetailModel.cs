fbc9ea0a04e227efb7e06c979290565ccd2347de
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 21:16:43 +0630

    Format Code

--- MTKDotNetCorePart2.PizzaApiDapperService/Model/PizzaOrderInvoiceDetailModel.cs
+++ MTKDotNetCorePart2.PizzaApiDapperService/Model/PizzaOrderInvoiceDetailModel.cs
@@ -1,10 +1,9 @@
-﻿namespace MTKDotNetCorePart2.PizzaApiDapperService.Model
+﻿namespace MTKDotNetCorePart2.PizzaApiDapperService.Model;
+
+public class PizzaOrderInvoiceDetailModel
 {
-    public class PizzaOrderInvoiceDetailModel
-    {
-        public int PizzaOrderInvoiceId { get; set;}
-        public string PizzaOrderInvoiceNo { get; set;}
-        public string PizzaExtraName {  get; set;}
-        public string Price { get; set; }
-    }
+    public int PizzaOrderInvoiceId { get; set;}
+    public string PizzaOrderInvoiceNo { get; set;}
+    public string PizzaExtraName {  get; set;}
+    public string Price { get; set; }
 }
