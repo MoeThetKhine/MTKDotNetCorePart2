13a6a317ea6a2e02ba2759a615b3a81be3daccf1
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 21:19:52 +0630

    Format Code

--- MTKDotNetCorePart2.PizzaApiDapperService/Model/PizzaExtraModel.cs
+++ MTKDotNetCorePart2.PizzaApiDapperService/Model/PizzaExtraModel.cs
@@ -1,19 +1,18 @@
-﻿namespace MTKDotNetCorePart2.PizzaApiDapperService.Model
+﻿namespace MTKDotNetCorePart2.PizzaApiDapperService.Model;
+
+[Table("Tbl_PizzaExtra")]
+public class PizzaExtraModel
 {
-    [Table("Tbl_PizzaExtra")]
-    public class PizzaExtraModel
-    {
-        [Key]
-        [Column("PizzaExtraId")]
-        public int PizzaExtraId { get; set; }
+    [Key]
+    [Column("PizzaExtraId")]
+    public int PizzaExtraId { get; set; }
 
-        [Column("PrizzaExtraName")]
-        public string PrizzaExtraName { get; set; }
+    [Column("PrizzaExtraName")]
+    public string PrizzaExtraName { get; set; }
 
-        [Column("Price")]
-        public decimal Price { get; set; }
+    [Column("Price")]
+    public decimal Price { get; set; }
 
-        [NotMapped]
-        public string PriceStr { get { return "$" + Price; } }
-    }
+    [NotMapped]
+    public string PriceStr { get { return "$" + Price; } }
 }
