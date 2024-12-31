0c22c72848011081430164e5e19292e02abe851d
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 21:24:41 +0630

    -

--- MTKDotNetCorePart2.PizzaApiDapperService/Model/OrderResponseModel.cs
+++ MTKDotNetCorePart2.PizzaApiDapperService/Model/OrderResponseModel.cs
@@ -1,8 +1,12 @@
 ﻿namespace MTKDotNetCorePart2.PizzaApiDapperService.Model;
 
+#region OrderResquestModel
+
 public class OrderResponseModel
 {
     public string Message { get; set; }
     public string InvoiceNo { get; set; }
     public decimal TotalAmount { get; set; }
 }
+
+#endregion
