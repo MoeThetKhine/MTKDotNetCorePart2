5f41b93eb4740cc42b9ba0af6b599bc228e246af
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 20:24:53 +0630

    -

--- MTKDotNetCorePart2.NLayer/DependencyInjection.cs
+++ MTKDotNetCorePart2.NLayer/DependencyInjection.cs
@@ -1,5 +1,7 @@
 ﻿namespace MTKDotNetCorePart2.NLayer;
 
+#region DependencyInjection
+
 public static class DependencyInjection
 {
     public static IServiceCollection AddFeatures(this IServiceCollection services)
@@ -7,3 +9,5 @@ public static class DependencyInjection
         return services.AddScoped<DA_Blog>().AddScoped<BL_Blog>();
     }
 }
+
+#endregion
