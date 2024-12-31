8506db1377e874223280099dfdc2bfc2810d3858
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 20:44:23 +0630

    -

--- MTKDotNetCorePart2.PickAPie/Models/PickAPieModel.cs
+++ MTKDotNetCorePart2.PickAPie/Models/PickAPieModel.cs
@@ -1,7 +1,11 @@
 ﻿namespace MTKDotNetCorePart2.PickAPie.Models;
 
+#region PickAPieModel
+
 public class PickAPieModel
 {
     public QuestionModel[] Questions { get; set; }
     public AnswerModel[] Answers { get; set; }
 }
+
+#endregion
