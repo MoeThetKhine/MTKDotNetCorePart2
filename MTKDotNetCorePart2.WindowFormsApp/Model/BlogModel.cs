ab89cfe2515e00a1790c84a80a513d2d296803a3
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 19:41:07 +0630

    Feat : Add Blog Model

---
+++ MTKDotNetCorePart2.WindowFormsApp/Model/BlogModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTKDotNetCorePart2.WindowFormsApp.Model
+{
+    public class BlogModel
+    {
+        public long BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
