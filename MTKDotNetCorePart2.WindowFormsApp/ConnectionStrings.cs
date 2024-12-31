a88b653b03836835e69a55fdaf389ba5b8ce9382
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 19:46:52 +0630

    Feat : Add ConnectionStrings

---
+++ MTKDotNetCorePart2.WindowFormsApp/ConnectionStrings.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+
+namespace MTKDotNetCorePart2.WindowFormsApp
+{
+    public class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder _sqlConnectionStringBuilder =
+            new()
+            {
+                DataSource = "localhost",
+                InitialCatalog = "OJTBatch1",
+                IntegratedSecurity = true,
+                TrustServerCertificate = true,
+            };
+    }
+}
