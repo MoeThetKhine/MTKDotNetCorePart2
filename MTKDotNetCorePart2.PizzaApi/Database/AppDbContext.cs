ea3d2912452dff21622af01d1f79f911f5bc36ea
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 20:59:39 +0630

    Format Code

--- MTKDotNetCorePart2.PizzaApi/Database/AppDbContext.cs
+++ MTKDotNetCorePart2.PizzaApi/Database/AppDbContext.cs
@@ -1,21 +1,19 @@
-﻿namespace MTKDotNetCorePart2.PizzaApi.Database
+﻿namespace MTKDotNetCorePart2.PizzaApi.Database;
+
+public class AppDbContext : DbContext
 {
-    public class AppDbContext : DbContext
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(ConnectionStrings._sqlConnectionStringBuilder.ConnectionString);
-        }
-
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-         : base(options)
-        {
-        }
-
-        public DbSet<PizzaModel> Pizzas { get; set; }
-        public DbSet<PizzaExtraModel>PizzaExtras { get; set; }
-        public DbSet<PizzaOrderDetailModel>PizzaOrderDetails { get; set; }
-        public DbSet<PizzaOrderModel>PizzaOrders { get; set; }
+        optionsBuilder.UseSqlServer(ConnectionStrings._sqlConnectionStringBuilder.ConnectionString);
     }
 
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+     : base(options)
+    {
+    }
+
+    public DbSet<PizzaModel> Pizzas { get; set; }
+    public DbSet<PizzaExtraModel>PizzaExtras { get; set; }
+    public DbSet<PizzaOrderDetailModel>PizzaOrderDetails { get; set; }
+    public DbSet<PizzaOrderModel>PizzaOrders { get; set; }
 }
