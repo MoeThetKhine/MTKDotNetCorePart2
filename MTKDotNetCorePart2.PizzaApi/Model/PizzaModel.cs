e38d916b9dc2fc67595e3b6553d97f12862e50dd
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 20:52:37 +0630

    -

--- MTKDotNetCorePart2.PizzaApi/Model/PizzaModel.cs
+++ MTKDotNetCorePart2.PizzaApi/Model/PizzaModel.cs
@@ -1,16 +1,16 @@
 ﻿namespace MTKDotNetCorePart2.PizzaApi.Model
 {
-        [Table("Tbl_Pizza")]
-        public class PizzaModel
-        {
-            [Key]
-            [Column("PizzaId")]
-            public int Id { get; set; }
+    [Table("Tbl_Pizza")]
+    public class PizzaModel
+    {
+        [Key]
+        [Column("PizzaId")]
+        public int Id { get; set; }
 
-            [Column("Pizza")]
-            public string Name {  get; set; }
+        [Column("Pizza")]
+        public string Name {  get; set; }
 
-            [Column("Price")]
-            public decimal Price {  get; set; }
-         }
+        [Column("Price")]
+        public decimal Price {  get; set; }
+     }
 }
