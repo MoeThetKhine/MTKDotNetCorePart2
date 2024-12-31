878cdccf44c7975a293c47cff4f660e1ca897489
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 19:41:27 +0630

    Feat : Add Blog Query

---
+++ MTKDotNetCorePart2.WindowFormsApp/Queries/BlogQuery.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTKDotNetCorePart2.WindowFormsApp.Queries
+{
+    public class BlogQuery
+    {
+        public static string BlogCreate { get; } = @"INSERT INTO [dbo].[Tbl_Blog]
+            ([BlogTitle]
+            ,[BlogAuthor]
+            ,[BlogContent])
+        VALUES
+            (@BlogTitle
+            ,@BlogAuthor
+            ,@BlogContent)";
+
+        public static string BlogList { get; } = @"SELECT [BlogId]
+        ,[BlogTitle]
+        ,[BlogAuthor]
+        ,[BlogContent]
+FROM [dbo].[Tbl_Blog]";
+            
+    }
+
+
+}
+
+
