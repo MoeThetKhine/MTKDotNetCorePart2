3c760b7c0bdf8ed4f2a53f9f3525f7be2d4ef726
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 21:50:49 +0630

    -

--- MTKDotNetCorePart2.WindowFormsApp/Program.cs
+++ MTKDotNetCorePart2.WindowFormsApp/Program.cs
@@ -1,17 +1,12 @@
-namespace MTKDotNetCorePart2.WindowFormsApp
+namespace MTKDotNetCorePart2.WindowFormsApp;
+
+internal static class Program
 {
-    internal static class Program
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new FrmMainMenu());
-        }
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+        Application.Run(new FrmMainMenu());
     }
 }
